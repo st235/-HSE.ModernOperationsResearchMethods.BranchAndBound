f3c4b6d1e8ac0f97794d259082283c96b214156b
Alexander Dadukin <hi@st235.xyz>
2023-02-27 08:14:20 +0000

    reduced graphs set according to the requirements.

--- branch_and_bound.cpp
+++ branch_and_bound.cpp
@@ -1241,18 +1241,16 @@ private:
 
 int main() {
     std::vector<std::string> files = { "brock200_1.clq", "brock200_2.clq", "brock200_3.clq", "brock200_4.clq",
-                                       "brock400_1.clq", "brock400_2.clq", "brock400_3.clq", "brock400_4.clq",
                                        "C125.9.clq",
                                        "gen200_p0.9_44.clq", "gen200_p0.9_55.clq",
                                        "hamming8-4.clq",
                                        "johnson16-2-4.clq", "johnson8-2-4.clq",
                                        "keller4.clq",
                                        "MANN_a27.clq", "MANN_a9.clq",
-                                       "p_hat1000-1.clq", "p_hat1000-2.clq",
-                                       "p_hat1500-1.clq",
-                                       "p_hat300-3.clq", "p_hat500-3.clq",
+                                       "p_hat1000-1.clq", "p_hat1500-1.clq",
+                                       "p_hat300-3.clq",
                                        "san1000.clq",
-                                       "sanr200_0.9.clq", "sanr400_0.7.clq"
+                                       "sanr200_0.9.clq"
     };
 
     std::ofstream fout("clique_bnb.csv");
